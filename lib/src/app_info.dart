89bcfa62504ef1688d84feb906a9caf0e706ac3e
David James McCorrie <james.mccorrie@lowrisc.org>
2026-07-28 22:33:31 +0100

    chore: release v0.7.15

--- lib/src/app_info.dart
+++ lib/src/app_info.dart
@@ -9,7 +9,7 @@
 /// be maintained by hand.
 library;
 
-const appVersion = '0.7.14+15';
+const appVersion = '0.7.15+16';
 
 /// One credited data source. [licence] is the terms under which Haqor
 /// redistributes it; [note] adds anything a reader should know beyond that.
